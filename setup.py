c3682e174378649c2261d7be941968c3b77603dc
nikhil <nikhil.vijaykumar@gmail.com>
2021-06-19 22:36:41 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='Exam',
+      version='1.0',
+      install_requires=[
+        'Flask',
+        'gensim',
+        'spacy',
+        #'git+https://github.com/boudinfl/pke.git',
+        'bert-extractive-summarizer',
+        'nltk',
+        'pdfminer',
+        'pywsd'
+    ]
+      )
